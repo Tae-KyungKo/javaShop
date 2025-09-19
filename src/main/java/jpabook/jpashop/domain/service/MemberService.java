cb660371f987f1eaf32cffc83cc79cfe2cd0f8d0
ttt103 <ttt103tk@gmail.com>
2025-09-19 22:11:21 +0900

    complete Item Service

---
+++ src/main/java/jpabook/jpashop/domain/service/MemberService.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.domain.service;
+
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    //final이 뭐고?
+    private final MemberRepository memberRepository;
+
+    //@Autowired  생성자 붙여놓으면 스프링이 자동으로 인젝션 해줌. 인젝션?
+    /*  RequireArgsConstructor 어노테이션 방법으로 대체 ???
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    */
+
+    //회원 가입
+    @Transactional(readOnly = false)    //기본적으로 false
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);    //중복 회원 검증 메서드
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    @Transactional(readOnly = true) //읽기만 할 때는 readOnly = true를 넣으면 성능이 좋아짐(쓰기에는 절대 안됨 update 불가)
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
---
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.domain.service;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+    @Test
+    //@Rollback(false)    //Transactional 어노테이션이 테스트 케이스에 있으면 롤백 해버려서 db에 들어가는거 안보임
+                            //왜? 테스트케이스는 사실 db에 남아있을 필요가 없으니까.
+                            //그래서 rollback(false)하면 db에 보임.
+    public void 회원가입() throws Exception{
+    Member member = new Member();
+    member.setName("kim");
+
+
+    Long savedId = memberService.join(member);
+
+    em.flush(); //transactional이 디비 비우기 전에 insert 쿼리 확인할 수 있도록 하는 용도?
+    assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //Given
+        Member member1 = new Member();
+        member1.setName("kim");
+        Member member2 = new Member();
+        member2.setName("kim");
+        //When
+        memberService.join(member1);
+        //Then
+        //IllegalStateException 예외가 발생하지 않으면 테스트 실패
+        assertThrows(IllegalStateException.class, () ->
+                memberService.join(member2));
+    }
+}
